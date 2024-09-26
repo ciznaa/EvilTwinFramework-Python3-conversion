7a215110a9a0c29444807dcf186fc65168f05b62
ciznaa <131717703+ciznaa@users.noreply.github.com>
2024-09-26 11:36:55 -0400

    Delete core directory

--- core/Spawners/ettercapspawner.py
+++
@@ -1,7 +0,0 @@
-from spawner import Spawner
-
-class EttercapSpawner(Spawner):
-
-    def __init__(self, config, name = "ettercap"):
-        super(EttercapSpawner, self).__init__(config, name)
-        self.calling = self.system_location + "/lua/core/ettercap.lua"
--- core/Spawners/mitmfspawner.py
+++
@@ -1,7 +0,0 @@
-from sslstripspawner import SSLStripSpawner
-
-class MITMFSpawner(SSLStripSpawner):
-
-    def __init__(self, config, name = "mitmf"):
-        super(MITMFSpawner, self).__init__(config, name)
-        self.calling = self.system_location + "/mitmf.py"
